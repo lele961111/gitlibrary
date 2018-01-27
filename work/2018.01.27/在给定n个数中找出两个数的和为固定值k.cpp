ee583b690a3b24d2f71021c6028f2012533ed45c
lele <2289923890@qq.com>
2018-01-28 01:25:33 +0800

    20180127，修改了判断输入的是否为数值的方法

--- work/2018.01.27/在给定n个数中找出两个数的和为固定值k.cpp
+++ work/2018.01.27/在给定n个数中找出两个数的和为固定值k.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include <cctype>
+#include<string>
 using namespace std;
 /*
 	今天算法题：在给定n个数中找出两个数的和为固定值k。
@@ -12,11 +13,16 @@ using namespace std;
 	
 	解析：
 		这道题可以先给他升序排序，然后从头和尾开始判断：
-		①.当头和尾相加大于k值时，说明尾数已经不再考虑范围了，因为它和最小的数组之和都比k大，和其他数加都打，所有尾数--;
-		②.当头和尾相加小于k值时，说明头数已经没用了，道理和①一样；
-		③.当头和尾相加等于k值，那就直接输出了，然后头++，尾--；
-		④.直到头和尾相遇结束。
-		排序算法，本案例采用快速排序。本案例还加了判断输入错误处理，get到两个方法：判断字符是否为数值isdigit(字符)， 将字符转为int型 int型=(int)(字符-'0')
+			①.当头和尾相加大于k值时，说明尾数已经不再考虑范围了，因为它和最小的数组之和都比k大，和其他数加都打，所有尾数--;
+			②.当头和尾相加小于k值时，说明头数已经没用了，道理和①一样；
+			③.当头和尾相加等于k值，那就直接输出了，然后头++，尾--；
+			④.直到头和尾相遇结束。
+		排序算法，本案例采用快速排序。
+		本案例还想加了判断输入错误处理：
+			get到两个方法：判断字符是否为数值isdigit(字符)， 将字符转为int型 int型=(int)(字符-'0')
+		    不过这两个字符方法对这个题来说局限性太大，必须保证输入的都是整数 且都是<10的数，所以就选择用string
+			根据判断字符串的每个字符来判定是否是数值  用string.c_str() 来讲string转换为char *型  再通过atoi()转为int   string->int: atoi(s.c_str())
+			就这样判断输入格式错误就解决了。
 		
 */
 
@@ -24,38 +30,49 @@ using namespace std;
 int size; //表示数组的长度
 int k; //表示固定值
 int * n; //指向数组的指针
-char c;
+//char c;
+string s;
 
+bool isNum(string s){ //判断是否为正确的数值
+	int i=0;
+	if(s[i]=='-'||(s[i]>='0'&&s[i]<='9'||s[i]=='+')){
+		for(++i;i<s.length()&&s[i]<='9'&&s[i]>='0';i++);	
+	}
+	if(i!=s.length()){
+		return false;
+	}
+	return true;
+}	
 
 //获取数据
 bool init(){
 	cout<<"请输入n：";
-	cin>>c;
-	if(!isdigit(c)){
+	cin>>s;
+	if(!isNum(s)){
 		cout<<"输入n值格式错误"<<endl;
 		return false;
 	}else{
-		size=(int)(c-'0');
+		size=atoi(s.c_str());
 	}
 	n=new int[size];
 	cout<<"请输入n个整数"<<endl;
 	for(int i=0;i<size;i++){
-		 cin>>c;
-		 if(!isdigit(c)){
+		 cin>>s;
+		 if(!isNum(s)){
 			cout<<"输入数组格式错误。"<<endl;
 			return false;
 		 }else{
-			n[i]=(int)(c-'0');
+			n[i]=atoi(s.c_str());
 		 }
 	}
 	cout<<endl;
 	cout<<"请输入k：";
-	cin>>c;
-	if(!isdigit(c)){
+	cin>>s;
+	if(!isNum(s)){
 		cout<<"输入k值格式错误。"<<endl;
 		return false;
 	}else{
-		k=(int)(c-'0');
+		k=atoi(s.c_str());
 	}
 	cout<<endl;
 	return true;
