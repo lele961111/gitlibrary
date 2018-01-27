9f0eda8dfb5e7773b582e86cac6d47950837e16a
lele <2289923890@qq.com>
2018-01-27 20:00:05 +0800

    20180126

--- work/2018.01.26/01串的最大子串.cpp
+++ work/2018.01.26/01串的最大子串.cpp
@@ -1,70 +1,54 @@
-#include <cstdio>
-#include <string>
-#include <stack>
-
+#include<iostream>
+#include<string>
 using namespace std;
+/*
+	题目：
+		如果一个01串任意两个相邻位置的字符都是不一样的，我们就叫这个01串为交错O1串。例如:"1","10101","0101010"都是交错01串。小易现在有一个01串s,
+	小易想找出一个最长的连续子串，并且这个子串是一个交错01串。小易需要你帮帮忙求出最长的这样的子串的长度是多少。
+	输入描述:
+		输入包括字符串s,s 的长度length(I s length < 50),字符串中只包含'0‘和'1'
+	输出描述:
+		输出一个整数，表示最长的满足要求的子串长度。
+	输入例子1:
+		111101111
+	输出例子1:
+		3
+*/
 
-bool hasPathCore(const char* matrix, int rows, int cols, int row, int col, const char* str, int& pathLength, bool* visited);
-
-bool hasPath(const char* matrix, int rows, int cols, const char* str)
-{
-    if(matrix == nullptr || rows < 1 || cols < 1 || str == nullptr)
-        return false;
-
-    bool *visited = new bool[rows * cols];
-    memset(visited, 0, rows * cols);
-
-    int pathLength = 0;
-    for(int row = 0; row < rows; ++row)
-    {
-        for(int col = 0; col < cols; ++col)
-        {
-            if(hasPathCore(matrix, rows, cols, row, col, str,
-                pathLength, visited))
-            {
-                return true;
-            }
-        }
-    }
-
-    delete[] visited;
-
-    return false;
+string s;
+int i;
+bool init(){ //c
+	bool b=true;
+	cout<<"请输入01串："<<endl;
+	getline(cin,s);
+	i=0;
+	for(;s[i]!='\0'&&(s[i]=='0'||s[i]=='1');i++); //用来遍历判读是否为为合格的01串
+	if(i!=s.length()){
+		cout<<"输入格式错误"<<endl;
+		b=false;
+	}
+	return b;
 }
 
-bool hasPathCore(const char* matrix, int rows, int cols, int row,
-    int col, const char* str, int& pathLength, bool* visited)
-{
-    if(str[pathLength] == '\0')
-        return true;
-
-    bool hasPath = false;
-    if(row >= 0 && row < rows && col >= 0 && col < cols
-        && matrix[row * cols + col] == str[pathLength]
-        && !visited[row * cols + col])
-    {
-        ++pathLength;
-        visited[row * cols + col] = true;
-
-        hasPath = hasPathCore(matrix, rows, cols, row, col - 1,
-            str, pathLength, visited)
-            || hasPathCore(matrix, rows, cols, row - 1, col,
-                str, pathLength, visited)
-            || hasPathCore(matrix, rows, cols, row, col + 1,
-                str, pathLength, visited)
-            || hasPathCore(matrix, rows, cols, row + 1, col,
-                str, pathLength, visited);
-
-        if(!hasPath)
-        {
-            --pathLength;
-            visited[row * cols + col] = false;
-        }
-    }
-
-    return hasPath;
+void findMax(){
+	int count1=1,count2=1;//count1表示当前最长子串， count2表示整个字符串中最长子串
+	for(int j=0;j<i-1;j++){
+		if(s[j]!=s[j+1]){
+			count1++;
+		}else{
+			count1=1;
+		}
+		count2=count1>count2?count1:count2;//三元表达式获取最长字符串
+	}
+	
+	cout<<"满足要求的子串长度："<<count2;
+	cout<<endl;
 }
 
-
-
-
+int main(void){
+	if(init()){
+		findMax();
+	}
+	
+	return 0;
+}
